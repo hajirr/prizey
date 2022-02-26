ca496e6764d527d2e1a0a6c537e06656c392e6f8
mac <mac@macs-MacBook-Pro.local>
2022-02-26 11:39:17 +0700

    feat: integrated with local api

--- script.js
+++ script.js
@@ -3,62 +3,80 @@ const refresh = document.getElementById("refresh");
 const prizeBox = document.getElementsByClassName("prize-box");
 const toAdd = document.createDocumentFragment();
 
-const data = [
-  "Muhajir",
-  "Nahda",
-  "Reza",
-  "Madsari",
-  "Muhayanah",
-  "Herdi",
-  "Ikbal",
-  "Oji",
-  "Tajoe",
-  "Ayat",
-  "Asep",
-  "Adan",
-  "Dika",
-  "Sisil",
-  "Dita",
-  "Yusril",
-  "Adika",
-];
-
-let newData = [];
-let pemenang = 10;
+let listHadiah = [];
+let listKaryawan = [];
+let listNip = [];
+let listPemenang = [];
 let isRoll = false;
-let myInterval = null;
+let myInterval = null;  
+
+const initData = () => {
+  for (var i = 0; i <= listHadiah.length - 1; i++) {
+    $(".prize-pool").append(
+      `<div class="prize-box"><img src="gift.png" alt="gift" /><p>${listHadiah[i]}</p></div>`
+    );
+  }
+  console.log("init data");
+};
+const getDataKaryawan = async () => {
+  const response = await fetch(`http://192.168.1.115:8000/api/karyawans`);
+  let data = await response.json();
+  data.map(function (e) {
+    listKaryawan.push(e.nama_karyawan);
+    listNip.push(e.nip);
+  });
+  console.log(data);
+};
+
+const getDataHadiah = async () => {
+  const response = await fetch(`http://192.168.1.115:8000/api/hadiahs`);
+  let data = await response.json();
+  data.map(function (e) {
+    listHadiah.push(e.nama_hadiah);
+  });
+  console.log(data);
+  initData();
+};
+
+getDataHadiah();
+getDataKaryawan();
 
 document.getElementById("start").onclick = function () {
-    if(!isRoll) {
-        myInterval = setInterval(roll, 100)
-        document.getElementById("start").innerHTML = ' Stop';
-        
+  if (listPemenang.length - 1 < listKaryawan.length) {
+    if (!isRoll) {
+      myInterval = setInterval(roll, 100);
+      document.getElementById("start").innerHTML = " Stop";
+      document.getElementById("start").className = "fa fa-stop";
+      document.getElementById("start").style.backgroundColor = "red";
     } else {
-        document.getElementById("start").innerHTML = ' Acak';
-        clearInterval(myInterval);
+      document.getElementById("start").innerHTML = " Start";
+      document.getElementById("start").className = "fa fa-play";
+      document.getElementById("start").style.backgroundColor = "#4caf50";
+      clearInterval(myInterval);
     }
     isRoll = !isRoll;
-}
+  } else {
+    alert("Pemenang harus lebih banyak dari hadiah");
+  }
+};
 
 function roll() {
-  if (pemenang < data.length) {
-    $(".prize-box").remove();
-    newData = [];
-    newData.push(data[Math.floor(Math.random() * data.length)]);
-    for (let i = 0; i < pemenang; i++) {
-      for (let j = 0; j < data.length; j++) {
-        const randomNumber = Math.floor(Math.random() * data.length);
-        if (!newData.includes(data[randomNumber])) {
-          newData.push(data[randomNumber]);
-        }
+  listPemenang = [];
+  $(".prize-box").remove();
+  listPemenang.push(
+    listKaryawan[Math.floor(Math.random() * listKaryawan.length)]
+  );
+  for (let i = 0; i < listPemenang.length; i++) {
+    for (let j = 0; j < listKaryawan.length; j++) {
+      const randomNumber = Math.floor(Math.random() * listKaryawan.length);
+      if (!listPemenang.includes(listKaryawan[randomNumber])) {
+        listPemenang.push(listKaryawan[randomNumber]);
       }
     }
-    for (var i = 0; i <= pemenang; i++) {
-      $(".prize-pool").append(
-        `<div class="prize-box"><img src="gift.png" alt="gift" /><p>${newData[i]}</p></div>`
-      );
-    }
-  } else {
-    alert("Peserta harus lebih banyak dari jumlah pemenang");
+  }
+  for (var i = 0; i <= listHadiah.length - 1; i++) {
+    $(".prize-pool").append(
+      `<div class="prize-box"><img src="gift.png" alt="gift" /><p>${listHadiah[i]}</p><p>${listPemenang[i]}</p></div>`
+    );
   }
 }
